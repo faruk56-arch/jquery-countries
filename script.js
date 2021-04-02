0839f14be19a472da792a8831b9019898b406053
momin <faruk373@gmail.com>
2021-04-02 23:59:34 +0200

    frontend

--- script.js
+++ script.js
@@ -7,11 +7,13 @@ $("button").click(function(){
         success: function(data, statuts, response) {
             
         
-            // $('p').html(data)
-            $('ul').html(data);
-
 
             
+            $('#country').html(data[0]);
+            $('#country1').html(data[1]);
+            $('#country2').html(data[2]);
+            $('#country3').html(data[3]);
+            $('#country4').html(data[4]);
             
             
             
